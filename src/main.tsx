9d1fa4004005d3e4ca19f727078388cb47020d11
AbdRahmen Amri <abdrahmen.3amri@gmail.com>
2025-01-11 00:12:29 +0100

    Add SEO component and integrate react-helmet-async for improved metadata management

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,19 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
+import { HelmetProvider } from 'react-helmet-async';
 import App from "./App.jsx"
 import "./index.css"
 
 const rootElement = document.getElementById("root");
 
 if (rootElement) {
-  ReactDOM.createRoot(rootElement).render(
-	<React.StrictMode>
-	  <App />
-	</React.StrictMode>
-  );
+	ReactDOM.createRoot(rootElement).render(
+		<React.StrictMode>
+			<HelmetProvider>
+				<App />
+			</HelmetProvider>
+		</React.StrictMode>
+	);
 } else {
-  console.error("Failed to find the root element");
+	console.error("Failed to find the root element");
 }
