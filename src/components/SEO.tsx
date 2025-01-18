cefe399d12f3377cc37db9d0e2b68f1021738497
AbdRahmen Amri <abdrahmen.3amri@gmail.com>
2025-01-19 00:23:06 +0100

    Add Open Graph image dimensions to SEO metadata

--- src/components/SEO.tsx
+++ src/components/SEO.tsx
@@ -27,6 +27,8 @@ export default function SEO({ title, description, name, type, keywords, image, u
             <meta property="og:description" content={description} />
             <meta property="og:url" content={url} />
             <meta property="og:image" content={image} />
+            <meta property="og:image:width" content="1200" />
+            <meta property="og:image:height" content="630" />
 
             { /* End Facebook tags */}
             { /* Twitter tags */}
